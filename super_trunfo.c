4915022b4a104c52f1816bfb491abf41277adafd
Ballack-333 <pedrohsgomes96@gmail.com>
2025-04-09 16:13:27 -0300

    super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+int main(){
+  char estado[8];
+  char codigo[4];
+  char cidade[]; //string
+  int populacao;
+  float area;
+  float pib;
+  float area;
+  int pontoturistico;
+
+printf ("Carta 1");
+printf ("Estado: \n")
+scanf ("%s", &estado);
+
+printf ("Código: \n");
+scanf ("%s", &codigo);
+
+printf ("Nome da Cidade: \n");
+scanf ("%s", &cidade);
+
+printf ("População: \n");
+scanf ("%f", &populacao);
+
+printf ("Área:  \n");
+scanf ("%.2f", &area);
+
+printf ("Número de Pontos Turísticos: \n");
+scanf ("%d", &pontoturistico);
+
+return 0;
+
+}
+  
+
+  
